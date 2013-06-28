4b7fecf20663a40ba362d8d50ac5ba3cfc02506b
L <506571722@qq.com>
2013-06-28 16:32:36 +0800

    fave detect v2.0

--- testdetecet.cpp
+++ testdetecet.cpp
@@ -14,53 +14,32 @@
 #ifdef _EiC
 #define WIN32
 #endif
- 
-static CvMemStorage* storage = 0;
-static CvHaarClassifierCascade* cascade = 0;
- 
-void detect_and_draw( IplImage* image, IplImage* img2 );
- 
-const char* cascade_name = "haarcascade_frontalface_alt.xml";
-/*    "haarcascade_profileface.xml";*/
- 
-int main(/* int argc, char** argv*/ )
+
+void detect( IplImage* image,CvMemStorage* storage, CvHaarClassifierCascade* cascade);
+void show_detecttime( double t);
+void draw(IplImage* img, CvSeq* faces, int i,double scale);
+
+int main( int argc, char** argv )
 {
+	CvMemStorage* storage = 0;
+	CvHaarClassifierCascade* cascade = 0;
     CvCapture* capture = 0;
-	IplImage *frame, *frame_temp, *frame_gray, *frame_copy = 0;
-    /*int optlen = strlen("--cascade=");
-    const char* input_name;*/
- 
-   /* if( argc > 1 && strncmp( argv[1], "--cascade=", optlen ) == 0 )
-    {
-        cascade_name = argv[1] + optlen;
-        input_name = argc > 2 ? argv[2] : 0;
-    }*/
-   /* else
-    {*/
-        cascade_name = "E:\\opencv\\data\\haarcascades\\haarcascade_frontalface_alt2.xml";
-        //opencv装好后haarcascade_frontalface_alt2.xml的路径,
-       //也可以把这个文件拷到你的工程文件夹下然后不用写路径名cascade_name= "haarcascade_frontalface_alt2.xml";  
-       //或者cascade_name ="C:\\Program Files\\OpenCV\\data\\haarcascades\\haarcascade_frontalface_alt2.xml"
-   /*     input_name = argc > 1 ? argv[1] : 0;
-    }*/
+	IplImage *frame, *frame_copy = 0;
+	char* cascade_name = "E:\\opencv\\data\\haarcascades\\haarcascade_frontalface_alt2.xml";
  
     cascade = (CvHaarClassifierCascade*)cvLoad( cascade_name, 0, 0, 0 );
+
+	//delete[] cascade_name;
+	cascade_name = NULL;
  
     if( !cascade )
     {
-       /* fprintf( stderr, "ERROR: Could not load classifier cascade\n" );
-        fprintf( stderr,
-        "Usage: facedetect --cascade=\"<cascade_path>\" [filename|camera_index]\n" );*/
         return -1;
     }
+
     storage = cvCreateMemStorage(0);
- 
-    //if( !input_name || (isdigit(input_name[0]) && input_name[1] == '\0') )
-        capture = cvCaptureFromCAM( 0 );
-  /*  else
-        capture = cvCaptureFromAVI( input_name ); */
-	
- 
+	capture = cvCaptureFromCAM( 0 );
+
     cvNamedWindow( "result", 1 );
  
     if( capture )
@@ -69,14 +48,9 @@ int main(/* int argc, char** argv*/ )
         {
             if( !cvGrabFrame( capture ))
                 break;
-            frame = cvRetrieveFrame( capture );
-			
-			frame_gray = cvCreateImage(cvGetSize(frame),IPL_DEPTH_8U,1);
-			cvCvtColor(frame,frame_gray,CV_BGR2GRAY);
-			frame_temp = frame;
-			frame = frame_gray;
 
-			//cvShowImage("frame_gray...", frame_gray ); 
+            frame = cvRetrieveFrame( capture );
+
             if( !frame )
                 break;
             if( !frame_copy )
@@ -86,83 +60,118 @@ int main(/* int argc, char** argv*/ )
                 cvCopy( frame, frame_copy, 0 );
             else
                 cvFlip( frame, frame_copy, 0 );
- 
-            detect_and_draw( frame_copy, frame_temp );
+
+            detect( frame_copy, storage, cascade);
  
             if( cvWaitKey( 10 ) >= 0 )
                 break;
         }
- 
-		cvReleaseImage( &frame );
+
+		cvReleaseHaarClassifierCascade(&cascade);
         cvReleaseImage( &frame_copy );
-		//cvReleaseImage( &frame_temp );
-		//cvReleaseImage( &frame_gray );
-        //cvReleaseCapture( &capture );
+		cvReleaseMemStorage( &storage );
+        cvReleaseCapture( &capture );
+
     }
     else
     {
         fprintf( stderr, "ERROR: Could not find a camera!\n" );
  
     }
-	cvReleaseCapture( &capture);
  
     cvDestroyWindow("result");
  
     return 0;
 }
  
-void detect_and_draw( IplImage* img, IplImage* img2 )
+void detect( IplImage* img ,CvMemStorage* storage,CvHaarClassifierCascade* cascade)
 {
-   /* static CvScalar colors[] = 
-    {
-        {{0,0,255}},
-        {{0,128,255}},
-        {{0,255,255}},
-        {{0,255,0}},
-        {{255,128,0}},
-        {{255,255,0}},
-        {{255,0,0}},
-        {{255,0,255}}
-    };*/
- 
-    double scale = 1.5;
-   // IplImage* gray = cvCreateImage( cvSize(img->width,img->height), 8, 1 );
+    double scale = 2;
+    IplImage* gray = cvCreateImage( cvSize(img->width,img->height), IPL_DEPTH_8U, 1 );
     IplImage* small_img = cvCreateImage( cvSize( cvRound (img->width/scale),
                          cvRound (img->height/scale)),
-                     8, 1 );
+                     IPL_DEPTH_8U, 1 );
 	
     int i;
  
-   /* cvCvtColor( img, gray, CV_BGR2GRAY );
-    cvResize( gray, small_img, CV_INTER_LINEAR );*/
+    cvCvtColor( img, gray, CV_BGR2GRAY );
+    cvResize( gray, small_img, CV_INTER_LINEAR );
 
-    cvResize( img, small_img, CV_INTER_LINEAR);
     cvEqualizeHist( small_img, small_img );
-	cvShowImage("small",small_img);
+	//cvShowImage("small",small_img);
     cvClearMemStorage( storage );
+
+			CvPoint frame_center;
+			frame_center.x = cvRound(img->width*0.5);
+			frame_center.y = cvRound(img->height*0.5);
+			cvCircle( img, frame_center, 64,cvScalar(255,0,0), 2, 8, 0 );
  
     if( cascade )
     {
         double t = (double)cvGetTickCount();
         CvSeq* faces = cvHaarDetectObjects( small_img, cascade, storage,
-                                            1.5, 1, CV_HAAR_DO_CANNY_PRUNING,
+                                            1.3, 1, CV_HAAR_DO_CANNY_PRUNING,
                                             cvSize(30, 30) );
-        t = (double)cvGetTickCount() - t;
-        printf( "detection time = %gms\n", t/((double)cvGetTickFrequency()*1000.) );
+        
+		show_detecttime(t);
+
         for( i = 0; i < (faces ? faces->total : 0); i++ )
         {
-            CvRect* r = (CvRect*)cvGetSeqElem( faces, i );
-            CvPoint center;
-            int radius;
-            center.x = cvRound((r->x + r->width*0.5)*scale);
-            center.y = cvRound((r->y + r->height*0.5)*scale);
-            radius = cvRound((r->width + r->height)*0.25*scale);
-			cvCircle( img2, center, radius, /*colors[i%8]*/cvScalar(0,0,255), 3, 8, 0 );
+           draw( img, faces, i, scale );
         }
     }
  
-    cvShowImage( "result", img2 );
-    //cvReleaseImage( &gray );
+    cvShowImage( "result", img );
+    cvReleaseImage( &gray );
     cvReleaseImage( &small_img );
 }
 
+void show_detecttime(double t )
+{
+	t = (double)cvGetTickCount() - t;
+    printf( "detection time = %gms\n", t/((double)cvGetTickFrequency()*1000.) );
+}
+
+void draw(IplImage* img, CvSeq* faces, int i, double scale)
+{
+	CvRect* r = (CvRect*)cvGetSeqElem( faces, i );
+    CvPoint face_center;
+	CvPoint frame_center;
+    int radius;
+	double x_gap, y_gap;
+	double x_rotate, y_rotate;
+	double threshold;
+
+    face_center.x = cvRound((r->x + r->width*0.5)*scale);
+    face_center.y = cvRound((r->y + r->height*0.5)*scale);
+    radius = cvRound((r->width + r->height)*0.2*scale);
+
+	cvCircle( img, face_center, radius,cvScalar(0,0,255), 3, 8, 0 );
+	cvCircle( img, face_center, 0,cvScalar(0,0,255), 3, 8, 0 );
+
+	frame_center.x = cvRound(img->width*0.25*scale);
+	frame_center.y = cvRound(img->height*0.25*scale);
+	cvCircle( img, frame_center, 1,cvScalar(0,255,0), 3, 8, 0 );
+	
+	x_gap = face_center.x - frame_center.x;
+	y_gap = -(face_center.y - frame_center.y);
+
+	int test = sqrt( x_gap*x_gap + y_gap*y_gap);
+
+	threshold = 64 - sqrt( x_gap*x_gap + y_gap*y_gap);
+
+	if(threshold<0)
+	{
+		x_rotate = 50 + (50 * (x_gap * 1.0)/640);
+
+		if( y_gap > 0 )
+		{
+			y_rotate = 100 * (y_gap * 1.0)/240;
+		}
+		else y_rotate = 0;
+
+		printf("水平转动：%lf；上下转动：%lf\n",x_rotate, y_rotate);
+	}
+	
+
+}
